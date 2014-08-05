c25eafe1d9f1f612106edaedb2d6ad648b89fc01
bobo__x <bobo__x@126.com>
2014-08-05 21:41:03 +0800

    加入星期几的切换。还有BUG

--- iOS/shop/view_iPhone/templates/2golf/Yudingbao/QiuchangTehui/QiuchangTehuiListBoard_iPhone.h
+++ iOS/shop/view_iPhone/templates/2golf/Yudingbao/QiuchangTehui/QiuchangTehuiListBoard_iPhone.h
@@ -29,10 +29,24 @@ AS_SIGNAL(LOCAL_RIGHT_NAV_BTN)
 @property (nonatomic,retain) IBOutlet UIButton* riziBtn;
 @property (nonatomic,retain) IBOutlet UIButton* jiagepaixuBtn;
 @property (nonatomic,retain) IBOutlet UIButton* shijianpaixuBtn;
+@property (retain, nonatomic) IBOutlet UIButton *btnmon;
+@property (retain, nonatomic) IBOutlet UIButton *btntwo;
+@property (retain, nonatomic) IBOutlet UIButton *btnwed;
+@property (retain, nonatomic) IBOutlet UIButton *btnthus;
+@property (retain, nonatomic) IBOutlet UIButton *btnfri;
+@property (retain, nonatomic) IBOutlet UIButton *btnsat;
+@property (retain, nonatomic) IBOutlet UIButton *btnsun;
 
 -(IBAction)onPressedShiduanBtn:(id)sender;
 -(IBAction)onPressedRiziBtn:(id)sender;
 -(IBAction)onPressedJiagepaixuBtn:(id)sender;
 -(IBAction)onPressedShijianpaixuBtn:(id)sender;
+- (IBAction)onPressedMon:(id)sender;
+- (IBAction)onPressedTwo:(id)sender;
+- (IBAction)onPressedWed:(id)sender;
+- (IBAction)onPressedThus:(id)sender;
+- (IBAction)onPressedFri:(id)sender;
+- (IBAction)onPressedSat:(id)sender;
+- (IBAction)onPressedSun:(id)sender;
 
 @end
