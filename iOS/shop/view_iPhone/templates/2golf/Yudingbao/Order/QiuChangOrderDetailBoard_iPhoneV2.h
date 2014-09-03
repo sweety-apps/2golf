7447358cc67e4b75c1df0402f734011216abdc58
bobo__x <bobo__x@126.com>
2014-09-03 19:53:12 +0800

    添加了球场订单详情的代码

---
+++ iOS/shop/view_iPhone/templates/2golf/Yudingbao/Order/QiuChangOrderDetailBoard_iPhoneV2.h
@@ -0,0 +1,13 @@
+//
+//  QiuChangOrderDetailBoard_iPhoneV2.h
+//  2golf
+//
+//  Created by rolandxu on 14-9-3.
+//  Copyright (c) 2014年 geek-zoo studio. All rights reserved.
+//
+
+#import "Bee_UIBoard.h"
+
+@interface QiuChangOrderDetailBoard_iPhoneV2 : BeeUIBoard
+
+@end
